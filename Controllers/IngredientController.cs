e5661f677cafc7c4b9904538eda0e593f12d613a
Jahaziel Higareda <jahaziel.higareda@outlook.com>
2019-11-18 10:27:42 +0000

    views: Ingredient, Category, Product, EditProfile, EditAddress

---
+++ Controllers/IngredientController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Tacovela.MVC.Models.Api;
+
+namespace Tacovela.MVC.Controllers
+{
+    public class IngredientController : BaseController
+    {
+        public IngredientController(IOptions<Api> enforcerApi) : base(enforcerApi) { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Tacovela.MVC.Models.Api;
+
+namespace Tacovela.MVC.Controllers
+{
+    public class ProductController : BaseController
+    {
+        public ProductController(IOptions<Api> enforcerApi) : base(enforcerApi) { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
